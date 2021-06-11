a01c38019155cc33d0bd986332855134b1a820f5
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-11 17:07:54 +0300

    add all new additions in the javascript document

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ var aname = function(year, month, date, gender) {
         return date && maleNames[date.getDay()];
     }
 }
+            //user interface logic:
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var year = parseInt($("#year").val());
+        var month = parseInt($("#month").val());
+        var date = parseInt($("#date").val());
+        var gender = $("input:radio[name=gender]:checked").val();
+        
+        var results = aname(year, month, date, gender);
+        alert("Your Akan name is: " + reset);
+
+                //reload page
+        document.getElementById("form").reset();
+    })
+})
+
