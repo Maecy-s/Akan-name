2355917fe6761fbbd0e5fdabd3df76fd9c3d30be
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-11 16:42:57 +0300

    add declared variable maleNames

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
             //back-end logic:
 var aname = function(year, month, date, gender) {
     var femaleNames = ["Akosua", "Andwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     
 }
