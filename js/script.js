686d1d9440d7135de9d425a41ce17128a272df37
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-11 16:50:04 +0300

    add declared variable date and an if else condition

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
 var aname = function(year, month, date, gender) {
     var femaleNames = ["Akosua", "Andwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    
+    var date = new Date(year, --month, date);
+    if (gender === "Female") {
+        return date && femaleNames[date.getDay()];
+    }
+    else {
+        return date && maleNames[date.getDay()];
+    }
 }
